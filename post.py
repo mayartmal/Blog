7718d1713023551e18e74b9c732d8333bc01ebcf
mayartmal <mayartmal@gmail.com>
2025-01-01 23:53:31 +0400

    - fixed post - 2 more tests for post

--- post.py
+++ post.py
@@ -4,3 +4,11 @@ class Post:
         self.content = content
 
 
+    def json(self):
+        return {
+            "title": self.title,
+            "content": self.content,
+        }
+
+    def __repr__(self):
+        return f"A {self.title} post record with {self.content} content"
--- tests/unit/test_post.py
+++ tests/unit/test_post.py
@@ -4,5 +4,17 @@ from post import Post
 class TestPost(TestCase):
     def test_create_post(self):
         post = Post("Test", "Test Content")
+
         self.assertEqual("Test", post.title)
         self.assertEqual("Test Content", post.content)
+
+    def test_json(self):
+        post = Post("Test", "Test Content")
+        expected_dict = {"title": "Test", "content": "Test Content"}
+
+        self.assertDictEqual(expected_dict, post.json())
+
+    def test_repr(self):
+        post = Post("Test", "Test Content")
+        expected_string = "A Test post record with Test Content content"
+        self.assertEqual(expected_string, post.__repr__())
