df28ad7101e0dbc8206f6a7959d70169ed03188e
mayartmal <mayartmal@gmail.com>
2025-01-02 00:16:12 +0400

    - fixed post - 2 more tests for post

---
+++ tests/unit/test_blog.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from blog import Blog
+
+class TestBlog(TestCase):
+    def test_create_blog(self):
+        blog = Blog("Test", "Test Author")
+
+        self.assertEqual("Test", blog.title)
+        self.assertEqual("Test Author", blog.author)
+        self.assertListEqual([], blog.posts)
